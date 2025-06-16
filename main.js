4709024f6da4eb68d933190653e89847b42f6d3a
insomnik1 <insomnik@gmail.com>
2025-06-16 16:15:52 +0200

    p3 algo

---
+++ main.js
@@ -0,0 +1,102 @@
+const prompt = require("prompt-sync")()
+
+let PlayerOne = {
+    namePlayer: "Sangoku",
+    pv: 100,
+    attack: [
+        {
+            nameAtk: "1: coup de tête, balayette, manchette",
+            power: -10,
+            accuracy: 1
+        },
+        {
+            nameAtk: "2: senzu",
+            power: 15,
+            accuracy: 2
+        },
+        {
+            nameAtk: "3: Kamehameha",
+            power: -20,
+            accuracy: 2
+        },
+        {
+            nameAtk: "4: Super KAMEHAMEHA",
+            power: -30,
+            accuracy: 3
+        }
+    ]
+}
+let PlayerTwo = {
+    namePlayer: "Vegeta",
+    pv: 100,
+    attack: [
+        {
+            nameAtk: "1 :Ne touche pas à ma Bulma",
+            power: -10,
+            accuracy: 1
+        },
+        {
+            nameAtk: "2: senzu",
+            power: 15,
+            accuracy: 2
+        },
+        {
+            nameAtk: "3: Big Bang Attack",
+            power: -20,
+            accuracy: 2
+        },
+        {
+            nameAtk: "4: Final FLASH pour Paul",
+            power: -30,
+            accuracy: 3
+        }
+    ]
+}
+
+function randomize(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+
+function choicePlayer() {
+    for (let i = 0; i < PlayerOne.attack.length; i++) {
+        console.log(PlayerOne.attack[i].nameAtk);
+    }
+
+    let choice = parseFloat(prompt("choississez une attaque"))
+    while (choice != "1" && choice != "2" && choice != "3" && choice != "4") {
+
+        choice = prompt("Attaque éronnée , merci de ne répondre que par 1, 2 , 3 ou 4")
+    }
+    return PlayerOne.attack[choice - 1]
+}
+
+
+function attaquePOne(attack, target, attaquant) {
+    choicePlayer
+    if (randomize(0, attack.accuracy) == attack.accuracy) {
+        if (attack.power > 0) {
+            attaquant.pv += attack.power
+            console.log("Senzu réussi, " + attaquant.namePlayer + " PV sont à présent de " + attaquant.pv);
+
+        } else {
+            target.pv += attack.power
+            console.log(target.namePlayer + " a été touché par " + attack.nameAtk + " ses pv restants sont " + target.pv);
+        }
+    } else {
+        console.log(attaquant.namePlayer + " Vous vous emmelez les pinceaux, attaque ratée !");
+    }
+    console.log("A l'issue de ce tour, vos pv restants sont de : " + target.namePlayer + target.pv);
+
+}
+
+
+while (PlayerOne.pv > 1 && PlayerTwo.pv > 1) {
+    let choice = choicePlayer()
+    attaquePOne(choice, PlayerTwo, PlayerOne)
+    let indexRandomAtk = randomize(0, PlayerTwo.attack.length - 1)
+    let atkPlayerTwo = PlayerTwo.attack[indexRandomAtk]
+    attaquePOne(atkPlayerTwo, PlayerOne, PlayerTwo)
+}
+
+return console.log("\ud83d\ude31 Fin de partie !!!! \ud83d\ude31" );
